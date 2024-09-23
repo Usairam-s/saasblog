c954dce0cf153eb4da65330e0b422326f90dbb86
Usairam-s <muhammaduseram@gmail.com>
2024-09-23 15:56:24 +0500

    api fix

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -7,27 +7,31 @@ export async function GET() {
   const user = await getUser();
 
   if (!user || user === null || !user.id) {
-    throw new Error("Error crearting user");
+    throw new Error("Something went wrong");
   }
 
   let dbUser = await prisma.user.findUnique({
     where: {
-      id: user?.id,
+      id: user.id,
     },
   });
 
   if (!dbUser) {
     dbUser = await prisma.user.create({
       data: {
-        id: user?.id,
-        email: user?.email ?? "",
-        firstName: user?.given_name ?? "",
-        lastName: user?.family_name ?? "",
+        id: user.id,
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        email: user.email ?? "",
         profileImage:
-          user?.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
       },
     });
   }
 
-  return NextResponse.redirect("http://localhost:3000/dashboard");
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "production"
+      ? "https://saasblog.vercel.app/dashboard"
+      : "http://localhost:3000/dashboard"
+  );
 }
